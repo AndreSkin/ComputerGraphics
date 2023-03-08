a110591d14095c766074f6f2ddfdf9fcce35d2a3
Andrea <80905987+AndreSkin@users.noreply.github.com>
2023-03-08 10:55:04 +0100

    Fixed cube

--- LAB_00/Cubo/Cubo/ShaderMaker.h
+++ LAB_00/Cubo/Cubo/ShaderMaker.h
@@ -5,10 +5,10 @@ using namespace std;
 
 class ShaderMaker
 {
-public:
-	static GLuint createProgram(char* vertexfilename, char* fragmentfilename);
-	static char* readShaderSource(const char* shaderFile);
-
-private:
-	ShaderMaker() { }
+    public:
+		static GLuint createProgram(char* vertexfilename, char *fragmentfilename);
+		static char* readShaderSource(const char* shaderFile);
+	
+    private:
+		ShaderMaker() { }	
 };
